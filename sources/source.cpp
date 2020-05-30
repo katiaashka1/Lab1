9a60d4ae4b3368cb280e4b0ffe535e2571cb4c2a
boikov3 <boikov090200@mail.ru>
2020-05-30 17:17:13 +0300

    gogo

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,14 +1,5 @@
-//Copyright 2019 <katiaashka1>
-#include "Json.hpp"
-
-
-int main() {
-        string text;
-        text = R"({ "lastname" : "Ivanov" , "firstname" : "Ivan" , "age" : 25,"islegal" : false,"marks" : [ 4 , 5 , 5 , 5 , 2 , 3 ],"address" : {  "city" : "Moscow" ,  "street" : "Vozdvijenka" } })";
-        Json object = Json::parse(text);
-        cout << std::endl;
-        if (object.is_object())
-            cout << "Yes";
-
+//Copyright 2020 <telsamar>
+int main()
+{
     return 0;
 }
